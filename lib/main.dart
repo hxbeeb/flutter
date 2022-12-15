d19ff44dac59b00367bacb396115c696f54c2149
GLORIENZE <habeebsalehalhussain@gmail.com>
2022-12-16 00:26:58 +0530

    app

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home:home(),));
+
+class home extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HOMELANDER'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+
+      ),
+      body: Center(
+        child:ElevatedButton.icon(onPressed:(){print('msg sent');}, icon: Icon(Icons.whatsapp),label:Text('send') ,
+        style:OutlinedButton.styleFrom(backgroundColor: Colors.black),
+        ),
+        ),
+      backgroundColor: Colors.deepPurple,
+      floatingActionButton: FloatingActionButton(
+        onPressed:() {print('u nigga');},
+        child: Text('+'),
+      ),
+    ) ;
+  }
+}
